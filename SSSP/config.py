898672d74754fb157b30d804b1da1c95824137df
mhibio <mhibio@gmail.com>
2024-10-08 19:29:02 +0900

    .config, type annotation 추가

--- SSSP/config.py
+++ SSSP/config.py
@@ -1,20 +1,20 @@
 from pydantic_settings import BaseSettings
 
 class Jwt():
-    secret_key = "develop_key"
-    algorithm = "HS256"
-    token_expire_minutes = 30
+    secret_key : str = "develop_key"
+    algorithm : str = "HS256"
+    token_expire_minutes : int = 30
 
 class Database():
-    filename = "test.db"
-    storage = f'./SSSP/storag/{filename}'
-    sqlite_url = f"sqlite:///{storage}"
+    filename : str = "test.db"
+    storage : str = f'./SSSP/storage/{filename}'
+    sqlite_url : str = f"sqlite:///{storage}"
 
 class Settings(BaseSettings):
-    app_name = "Soongsil Security Playground"
-    favicon_path = './static/favicon.ico'
-    
-    jwt = Jwt()
-    database = Database()
+    app_name : str = "Soongsil Security Playground"
+    favicon_path : str = './static/favicon.ico'
+
+    jwt : Jwt = Jwt()
+    database : Database = Database()
 
 settings = Settings()
