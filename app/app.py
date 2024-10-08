1250a0aa19bd3221ff00c2e510c69c2faea18410
mhibio <mhibio@gmail.com>
2024-10-08 14:58:22 +0900

    favicon 추가 && gitignore 설정

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+
 from app.database import engine
 from app import models
 
@@ -11,3 +13,9 @@ models.Base.metadata.create_all(bind=engine)
 apimain = FastAPI()
 
 apimain.include_router(users.router)
+
+favicon_path = './static/favicon.ico'
+
+@apimain.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return FileResponse(favicon_path)
