ca59beff5bdfbcfe6a5fc4ce86e202edaaaf8e3a
JungTae Kwon <oxdjww@gmail.com>
2024-11-01 23:30:32 +0900

    REFACTOR: user contents column 추가

--- SSSP/api/routers/v1/auth/register.py
+++ SSSP/api/routers/v1/auth/register.py
@@ -9,6 +9,8 @@ from SSSP.api.core import auth
 from SSSP.api.core.database import *
 
 router = APIRouter()
+
+
 @router.post("/register", response_model=schema_users.User)
 def register(user: schema_users.UserCreate, db: Session = Depends(get_db)):
     user_in_db = db.query(models.User).filter(models.User.email == user.email).first()
@@ -16,11 +18,11 @@ def register(user: schema_users.UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Email already registered")
 
     hashed_password = auth.get_password_hash(user.password)
-
     new_user = models.User(
         username=user.username,
         email=user.email,
-        hashed_password=hashed_password
+        hashed_password=hashed_password,
+        contents=user.contents,
     )
     db.add(new_user)
     db.commit()
--- SSSP/api/schemas/schema_users.py
+++ SSSP/api/schemas/schema_users.py
@@ -1,14 +1,18 @@
 from pydantic import BaseModel, EmailStr
 
+
 class UserCreate(BaseModel):
     username: str
     email: EmailStr
     password: str
+    contents: str
+
 
 class User(BaseModel):
     id: int
     username: str
     email: EmailStr
+    contents: str
 
     class Config:
         from_attributes = True
