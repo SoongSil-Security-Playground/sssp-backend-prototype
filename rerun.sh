6c7e64a3620e55ad15f3f654ba0ba4441267b3b6
oxdjww <oxdjww@gmail.com>
2024-11-26 04:15:38 +0000

    CHORE: Update script to controll be

--- rerun.sh
+++ rerun.sh
@@ -1,5 +1,5 @@
 # docker-compose down -v
 # docker-compose up -d --build
-docker-compose stop sssp-backend
-docker-compose rm -f sssp-backend
-docker-compose up -d --build sssp-backend
+docker stop sssp-backend
+docker rm -f sssp-backend
+docker-compose up -d --build
