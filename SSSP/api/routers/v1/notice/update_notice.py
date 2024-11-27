432a7306c55b41f5b9f7496bcd3efe18e74f90ab
oxdjww <oxdjww@gmail.com>
2024-11-27 21:07:18 +0900

    FIX: Fix to find id in notice table

--- SSSP/api/routers/v1/notice/update_notice.py
+++ SSSP/api/routers/v1/notice/update_notice.py
@@ -12,16 +12,19 @@ from SSSP.api.schemas import schema_notice
 
 
 import logging
+
 logging.basicConfig(level=logging.INFO)
 
 router = APIRouter()
+
 
 @router.patch("/notice")
 def update_notice(
     notice_id: int,
     title: str = Form(None),
     content: str = Form(None),
-    token: str = Depends(settings.oauth2_scheme), db: Session = Depends(get_db)
+    token: str = Depends(settings.oauth2_scheme),
+    db: Session = Depends(get_db),
 ):
     user = get_current_user_by_jwt(token, db)
     if user.authority != UserRole.ADMIN:
@@ -30,24 +33,22 @@ def update_notice(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Not authorized to update notice",
         )
-    
-    notice = (
-        db.query(models.Notice).filter(models.Challenge.id == notice_id).first()
-    )
+
+    notice = db.query(models.Notice).filter(models.Notice.id == notice_id).first()
 
     if not notice:
         logging.warning(f"Notice not found for update: ID {notice_id}")
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Notice not found"
         )
-    
+
     if title is not None:
         notice.title = title
 
     if content is not None:
-        notice.title = content
+        notice.content = content
 
     db.commit()
     db.refresh(notice)
 
-    return {'success':1}
+    return {"success": 1}
