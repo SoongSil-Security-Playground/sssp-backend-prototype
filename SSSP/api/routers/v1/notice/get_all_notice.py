fb8444e1beb54d8d8f68f20870690ad8ade92e51
oxdjww <oxdjww@gmail.com>
2024-11-27 12:23:20 +0000

    CHORE: formatting

--- SSSP/api/routers/v1/notice/get_all_notice.py
+++ SSSP/api/routers/v1/notice/get_all_notice.py
@@ -8,9 +8,11 @@ from SSSP.api.core.database import *
 from SSSP.api.core.auth import get_current_user_by_jwt
 
 import logging
+
 logging.basicConfig(level=logging.INFO)
 
 router = APIRouter()
+
 
 @router.get("/notice", response_model=list[schema_notice.NoticeResponse])
 def get_all_notice(
@@ -19,7 +21,9 @@ def get_all_notice(
     user = get_current_user_by_jwt(token, db)
 
     notice_list = db.query(models.Notice).all()
-    notice_response = [schema_notice.NoticeResponse.from_orm(notice) for notice in notice_list]
+    notice_response = [
+        schema_notice.NoticeResponse.from_orm(notice) for notice in notice_list
+    ]
     logging.info(f"[*] Notice List >> {notice_response}")
 
     return notice_response[::-1]
